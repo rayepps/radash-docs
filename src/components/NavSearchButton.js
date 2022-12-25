fc4e7f0a9d4226fc31ff395f98720b73c0271217
Nguyen Cao Cuong - Peter James <50004497+caocuong2404@users.noreply.github.com>
2022-12-26 05:06:35 +0700

    update: css style `NavSearchButton` and add keyboard shortcut trigger focus (#49)

--- src/components/NavSearchButton.js
+++ src/components/NavSearchButton.js
@@ -40,6 +40,15 @@ export function NavSearchButton({ children, ...props }) {
         focus()
       }
 
+      // Shortcut to focus on search input
+      if (
+        (event.keyCode === 27) ||
+        (event.key === 'k') ||
+        (event.key === '/')
+      ) {
+        focus()
+      }
+
       pressed = event
     }
 
@@ -62,43 +71,47 @@ export function NavSearchButton({ children, ...props }) {
   return (
     <div
       onClick={focus}
-      className="flex p-2 items-center rounded border border-slate-200 hover:border-slate-300"
+      className="relative w-full flex items-center"
     >
-      <svg
-        width="24"
-        height="24"
-        fill="none"
-        aria-hidden="true"
-        className="mr-3 flex-none"
-      >
-        <path
-          d="m19 19-3.5-3.5"
-          strokeWidth="2"
-          className="stroke-slate-400"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <circle
-          cx="11"
-          cy="11"
-          r="6"
-          strokeWidth="2"
-          className="stroke-slate-400"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <svg
+          width="24"
+          height="24"
+          fill="none"
+          aria-hidden="true"
+          className="mr-3 flex-none"
+        >
+          <path
+            d="m19 19-3.5-3.5"
+            strokeWidth="2"
+            className="stroke-slate-400"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <circle
+            cx="11"
+            cy="11"
+            r="6"
+            strokeWidth="2"
+            className="stroke-slate-400"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+      </div>
       <input
-        className="grow w-full"
+        className="pl-10 p-2.5 grow w-full rounded border border-slate-200 hover:border-slate-300"
         type="text"
         ref={inputRef}
         onChange={onInput}
         placeholder="Quick search..."
       />
       {actionKey && (
-        <span className="ml-auto pl-3 flex-none text-xs font-semibold text-slate-400">
-          {actionKey[0]}K
-        </span>
+        <div className="absolute inset-y-0 right-0 flex items-center pr-3">
+          <span className="ml-auto pl-3 flex-none text-xs font-semibold text-slate-400">
+            {actionKey[0]}K
+          </span>
+        </div>
       )}
     </div>
   )
